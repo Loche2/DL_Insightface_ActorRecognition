c6de3ab3b008ab7ace0bacaefbc25f01321921bc
LocheZ <1913565532@qq.com>
2023-10-22 00:32:46 +0800

    Initial Commit

--- test_initial.py
+++ test_initial.py
@@ -1,6 +1,4 @@
 import cv2
-import numpy as np
-import insightface
 from insightface.app import FaceAnalysis
 from insightface.data import get_image as ins_get_image
 
--- test_recognition.py
+++ test_recognition.py
@@ -1,5 +1,3 @@
-import cv2
-import numpy as np
 import insightface
 from insightface.app import FaceAnalysis
 from insightface.data import get_image as ins_get_image
